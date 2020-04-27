3ff703fe6c05be824471249687d1345e6ffa51b5
Marta Verdugo <marta_v_m6@hotmail.com>
2020-04-27 08:29:19 +0000

    backend finalizado

--- tourismAPI/index.js
+++ tourismAPI/index.js
@@ -13,101 +13,64 @@ module.exports = function(app){
 
 	
 	const BASE_API_URL = "/api/v1";
+	
+	var initialTourism =  [{"province": "almeria","year": 2015,"traveller": 11260,"overnightstay":37406,"averagestay": 3.3},
+					{"province": "cadiz","year": 2015,"traveller": 28859,"overnightstay":77652,"averagestay": 2.7},
+					{"province": "cordoba","year": 2015,"traveller": 22365,"overnightstay":76373,"averagestay": 3.4},
+					{"province": "granada","year": 2015,"traveller": 23873,"overnightstay":67636,"averagestay": 2.8},
+					{"province": "huelva","year": 2015,"traveller": 40651,"overnightstay":90601,"averagestay": 2.2},
+					{"province": "jaen","year": 2015,"traveller": 23513,"overnightstay":63311,"averagestay": 2},
+					{"province": "malaga","year": 2015,"traveller": 56208,"overnightstay":301760,"averagestay": 5.4},
+					{"province": "sevilla","year": 2015,"traveller": 22454,"overnightstay":55880,"averagestay": 2.5},];
 
-	var initialTourism = [{
-		"province": "almeria",
-		"year": 2015,
-		"traveller": 11260,
-		"overnightstay":37406,
-		"averagestay": 3.3
-	},{
-		"province": "cadiz",
-		"year": 2015,
-		"traveller": 28859,
-		"overnightstay":77652,
-		"averagestay": 2.7
-	},{
-		"province": "cordoba",
-		"year": 2015,
-		"traveller": 22365,
-		"overnightstay":76373,
-		"averagestay": 3.4
-	},{
-		"province": "granada",
-		"year": 2015,
-		"traveller": 23873,
-		"overnightstay":67636,
-		"averagestay": 2.8
-	},{
-		"province": "huelva",
-		"year": 2015,
-		"traveller": 40651,
-		"overnightstay":90601,
-		"averagestay": 2.2
-	},{
-		"province": "jaen",
-		"year": 2015,
-		"traveller": 23513,
-		"overnightstay":63311,
-		"averagestay": 2
-	},{
-		"province": "malaga",
-		"year": 2015,
-		"traveller": 56208,
-		"overnightstay":301760,
-		"averagestay": 5.4
-	},{
-		"province": "sevilla",
-		"year": 2015,
-		"traveller": 22454,
-		"overnightstay":55880,
-		"averagestay": 2.5
-	}];
 
 	//LOADINITIALDATA
 	app.get(BASE_API_URL + "/rural-tourism-stats/loadInitialData", (req, res) => {
 		console.log("New GET ..../loadInitialData");
+		db.remove({},{multi:true});
 		db.insert(initialTourism);
 		res.sendStatus(200);
-		console.log("Initial Contact loaded"+JSON.stringify(contacts,null,2));
+		console.log("Initial Contact loaded"+JSON.stringify(initialTourism,null,2));
+		
 	});
 
 	//GET /rural-tourism-stats
 
 	app.get(BASE_API_URL + "/rural-tourism-stats", (req,res)=>{
-		console.log("New GET ..../rural-tourism-stats");
-		db.find({}, (error, tourism) =>{
+		var dbquery = {};
+        let offset = 0;
+        let limit = Number.MAX_SAFE_INTEGER;
+		
+		//PAGINACIÓN
+        if (req.query.offset) {
+            offset = parseInt(req.query.offset);
+            delete req.query.offset;
+        }
+        if (req.query.limit) {
+            limit = parseInt(req.query.limit);
+            delete req.query.limit;
+        }
+		
+		//BUSQUEDA
+		if(req.query.province) dbquery["province"]= req.query.province;
+		if(req.query.year) dbquery["year"] = parseInt(req.query.year);
+		if(req.query.traveller) dbquery["traveller"] = parseFloat(req.query.traveller);
+		if(req.query.overnightstay) dbquery["overnightstay"] = parseFloat(req.query.overnightstay);
+		if(req.query.averagestay) dbquery["averagestay"] = parseFloat(req.query.averagestay);	
+		
+		db.find(dbquery).sort({province:1,year:-1}).skip(offset).limit(limit).exec((error, tourism) =>{
 
 			tourism.forEach((t)=>{
 				delete t._id
 			});
 
 			res.send(JSON.stringify(tourism,null,2));
-			console.log("Data sent: " + JSON.stringify(tourism,null,2));
+			//console.log("Data sent: " + JSON.stringify(tourism,null,2));
+			console.log("Recursos mostrados");
 		});
-		
 	});
 
-
-	//GET /rural-tourism-stats/XXX
-
-	app.get(BASE_API_URL+"/rural-tourism-stats/:province", (req,res)=>{
-
-		var province = req.params.province;
-
-		db.find({"province" :province},(error, tourism)=>{
-			if(tourism.length==0){
-				console.log("ERROR 404. Recurso no encontrado");
-				res.sendStatus(404);
-			}else{
-				res.send(tourism.map((t)=>{
-					delete t._id;
-					return(t);
-				}));
-				console.log("Data sent: " + JSON.stringify(tourism,null,2));
-			}
-		})
-	});
+	
 	//GET /rural-tourism-stats/XXX/YYY
 
 	app.get(BASE_API_URL+"/rural-tourism-stats/:province/:year", (req,res)=>{
@@ -123,8 +86,9 @@ module.exports = function(app){
 				res.send(tourism.map((t)=>{
 					delete t._id;
 					return(t);
-				}));
-				console.log("Data sent: " + JSON.stringify(tourism,null,2));
+				})[0]);
+				//console.log("Data sent: " + JSON.stringify(tourism,null,2));
+				console.log("Recurso mostrado");
 			}
 		})
 	});
@@ -168,18 +132,19 @@ module.exports = function(app){
 				console.log("Error 404, recurso no encontrado.");
 				res.sendStatus(404);
 			}else if(!updateTourism.province || !updateTourism.year ||!updateTourism.traveller || !updateTourism.overnightstay
-		  			 || !updateTourism.averagestay || updateTourism.province != province || updateTourism.year != year
-					 || Object.keys(updateTourism).length != 5){
+		  			 || !updateTourism.averagestay || Object.keys(updateTourism).length != 5){
 				
 					console.log("PUT recurso encontrado. Se intenta actualizar con campos no validos 400");
 					res.sendStatus(400);
+			}else if(updateTourism.province != province || updateTourism.year != year){
+					console.log("PUT recurso encontrado. Se intenta actualizar el identificador 409");
+					res.sendStatus(409);
 			}else{
 				db.update({"province":province,"year":year},{$set: updateTourism});
 				console.log("PUT realizado con exito.")
 				res.sendStatus(200);
 			}
 		});
-	
 	});
 
 	// DELETE /rural-tourism-stats
@@ -192,23 +157,6 @@ module.exports = function(app){
 		console.log("Todos los recursos han sido eliminados");
 	});
 
-	// DELETE /rural-tourism-stats/XXX
-
-	app.delete(BASE_API_URL+"/rural-tourism-stats/:province", (req,res)=>{
-
-		var province = req.params.province;
-
-		db.find({"province":province},(error, tourism)=>{
-			if(tourism.length==0){
-				console.log("ERROR 404. Recurso no encontrado");
-				res.sendStatus(404);
-			}else{
-				console.log("DELETE de un recurso concreto");
-                res.sendStatus(200);
-                db.remove({ "province":province });
-			}
-		})
-	});
 	
 	// DELETE /rural-tourism-stats/XXX/YYY
 
@@ -231,6 +179,10 @@ module.exports = function(app){
 
 	//POST incorrecto
 	app.post(BASE_API_URL + "/rural-tourism-stats/:province", (req, res) => {
+		res.sendStatus(405);
+	});
+	//POST incorrecto
+	app.post(BASE_API_URL + "/rural-tourism-stats/:province/:year", (req, res) => {
 		res.sendStatus(405);
 	});
 
